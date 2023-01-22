e5a20f364eed3dd3a671afea2730d36ccaf3399a
Alex Walker <alex.walker@indicio.tech>
2023-01-22 15:39:56 +0100

    feat: adding routes
    Signed-off-by: Alex Walker <alex.walker@indicio.tech>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import "./App.css";
 
-import "./routes/home/home.styles.scss";
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/home.component";
+
+const Menu = () => {
+  return <h1>Menu page</h1>;
+};
 
 const App = () => {
   return (
-    <div className="outer-container">
-      <img
-        class="background-img"
-        src="https://gcdnb.pbrd.co/images/0AenMkzPJszt.png?o=1"
-      />
-
-      <button className="top-menu btn">Menu</button>
-      <button className="top-order-now btn">Order Now</button>
-
-      <button className="menu btn">menu</button>
-      <button className="locations btn">locations</button>
-      <button className="order btn">place order</button>
-      <button className="contact btn">contact</button>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/menu" element={<Menu />} />
+    </Routes>
   );
 };
 
---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,40 @@
+import "./home.styles.scss";
+
+const Home = () => {
+  return (
+    <div className="outer-container">
+      <img
+        className="background-img"
+        src="https://cdn.discordapp.com/attachments/1065884142417498145/1066675467492532264/hana-bg-final.png"
+      />
+
+      <button className="top-menu btn">Menu</button>
+      <button className="top-order-now btn">Order Now</button>
+
+      <button className="menu btn">menu</button>
+      <img
+        className="menu-img img-icon"
+        src="https://cdn.discordapp.com/attachments/1065884142417498145/1066675856350654475/hana-png-1.png"
+      />
+
+      <button className="locations btn">locations</button>
+      <img
+        className="locations-img img-icon"
+        src="https://cdn.discordapp.com/attachments/1065884142417498145/1066675856933662740/hana-png-4.png"
+      />
+      <button className="order btn">place order</button>
+      <img
+        className="order-img img-icon"
+        src="https://cdn.discordapp.com/attachments/1065884142417498145/1066675856535199784/hana-png-2.png"
+      />
+
+      <button className="contact btn">contact</button>
+      <img
+        className="contact-img img-icon"
+        src="https://cdn.discordapp.com/attachments/1065884142417498145/1066675856732323840/hana-png-3.png"
+      />
+    </div>
+  );
+};
+
+export default Home;
